ea5b96e08b0d32581b9620cbd722f308641af423
pabloabur <pabloabur@gmail.com>
2020-11-13 20:23:40 +1100

    Changes for compatibility with vim navigation between windows

--- config.h
+++ config.h
@@ -192,8 +192,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,            	XK_u,     	kscrollup,      {.i = -1} },
 	{ MODKEY,            	XK_d,   	kscrolldown,    {.i = -1} },
-	{ MODKEY,            	XK_k,     	kscrollup,      {.i = +1} },
-	{ MODKEY,            	XK_j,     	kscrolldown,    {.i = +1} },
+	{ MODKEY,            	XK_y,     	kscrollup,      {.i = +1} },
+	{ MODKEY,            	XK_e,     	kscrolldown,    {.i = +1} },
 };
 
 /*
